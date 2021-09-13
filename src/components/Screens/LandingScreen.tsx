f3a9481c310c8e1e003ff5e32a423af13aef8a74
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2021-09-13 22:58:22 +0100

    feat: added one central location for value changes

--- src/components/Screens/LandingScreen.tsx
+++ src/components/Screens/LandingScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './LandingScreen.module.scss'
+import colors from '../../theme/colors.module.scss';
 
 const LandingScreen: React.FC = () => {
     return (
         <div className='landing-screen'>
-            landing screen
+            <p>myRed's value: {colors.myRed}</p>
         </div>
     )
 }
