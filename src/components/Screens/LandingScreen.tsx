2e44a6e012a7963917dde6dd66fbe36efd8b6dd7
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2021-09-16 23:35:39 +0100

    fix: prettier added

--- src/components/Screens/LandingScreen.tsx
+++ src/components/Screens/LandingScreen.tsx
@@ -7,7 +7,7 @@ const LandingScreen: React.FC = () => {
     return (
         <div className={styles['landing-screen']}>
                 <LandingScreenHeader />
-            <button className="landing-screen__scroll">
+            <button className={styles['landing-screen__scroll']}>
 
             </button>
         </div>
@@ -18,9 +18,9 @@ const LandingScreenHeader: React.FC = () => {
 
     return (
         <div className={styles.header}>
-            <h2 className="header__top">Developer</h2>
-            <h1 className="header__middle">Michael Taiwo<span className='header__dot'>.</span></h1>
-            <h2 className="header__bottom">Designer</h2>
+            <h2 className={styles['header__top']}>Developer</h2>
+            <h1 className={styles['header__middle']}>Michael Taiwo<span className={styles['header__dot']}>.</span></h1>
+            <h2 className={styles['header__bottom']}>Designer</h2>
         </div>
     )
 }
