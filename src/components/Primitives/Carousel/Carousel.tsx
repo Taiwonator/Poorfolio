3c38f7129855b907c055234ece4f9624f63ff81f
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2021-11-01 23:38:23 +0000

    feat(carousel): unfinished carousel added

--- src/components/Primitives/Carousel/Carousel.tsx
+++ src/components/Primitives/Carousel/Carousel.tsx
@@ -1,37 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Carousel.module.scss'
+import classNames from 'classnames'
+import useEmblaCarousel from 'embla-carousel-react'
+import useAnimationFrame from 'src/hooks/useAnimationFrame'
+
+const urlMap = {
+    '/flux.png': '/flux--full.png',
+    '/certax.png': '/certax--full.png', 
+    '/pets.png': '/pets--full.png', 
+}
 
 const Carousel = props => {
+    const [modalVisible, setModalVisibility] = useState(false)
+    const [modalUrl, setModalUrl] = useState('/flux--full.png')
+    
+    const openModal = (url) => {
+        setModalUrl(url)
+        setModalVisibility(true)
+    }
+
+    const closeModal = () => {
+        setModalVisibility(false)
+    }
+    
     return (
-        // <div className={styles['carousel-container']}>
-        //     <Item url='/flux.png'/>
-        //     {/* <Item url='/certax.png'/>
-        //     <Item url='/pets.png'/> */}
-        // </div>
-        <EmblaCarousel />
+        <>
+            <EmblaCarousel openModal={openModal} />
+            <Modal closeModal={closeModal} isVisible={modalVisible} url={modalUrl} />
+        </>
     )
 }
 
-const Item = ({ url }) => {
+const Item = ({ url, openModal }) => {
+    const onMouseUp = () => {
+        openModal(urlMap[url])
+    }
+
     return (
-        <img className={styles['carousel__item']} src={url} alt="website screenshot" />
+        <img
+            onMouseUp={() => onMouseUp()}
+            className={styles['carousel__item']}
+            src={url} alt="website screenshot"
+        />
     )
 }
 
-export default Carousel
+const Modal = ({ closeModal, isVisible, url }) => {
+    const className = classNames(
+        styles['modal'], 
+        !isVisible && styles['modal--hidden']
+    )
 
-import useEmblaCarousel from 'embla-carousel-react'
+    return (
+        <div className={className}>
+            <img
+                className={styles['modal__img']}
+                alt="full website screenshot"
+                src={url}
+            />
+            <div className={styles['modal__background']} onClick={() => closeModal()}/>
+        </div>
+    )
+}
 
-export const EmblaCarousel = () => {
+interface EmblaCarouselProps {
+    openModal: (url) => void
+}
+
+const EmblaCarousel: React.FC<EmblaCarouselProps> = ({ openModal }) => {
   const [emblaRef] = useEmblaCarousel()
 
   return (
     <div className={styles['embla']} ref={emblaRef}>
       <div className={styles['embla__container']}>
-        <div className={styles['embla__slide']}><Item url='/flux.png'/></div>
-        <div className={styles['embla__slide']}><Item url='/certax.png'/></div>
-        <div className={styles['embla__slide']}><Item url='/pets.png'/></div>
+        <div className={styles['embla__slide']}><Item openModal={openModal} url='/flux.png'/></div>
+        <div className={styles['embla__slide']}><Item openModal={openModal} url='/certax.png'/></div>
+        <div className={styles['embla__slide']}><Item openModal={openModal} url='/pets.png'/></div>
       </div>
     </div>
   )
 }
+
+export default Carousel
